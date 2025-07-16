ee66016dbe6157af6ea29e3baea05b68fe84d904
Vaman <154677850+Ducmanh2712@users.noreply.github.com>
2025-07-16 20:01:13 +0700

    Update pre_chroot_archinstall_efi.sh
    Fuk this sh*t I'm out

--- pre_chroot_archinstall_efi.sh
+++ pre_chroot_archinstall_efi.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 set -e
 
-# Change the partition name 
 DISK="/dev/sda"
 EFI_PART="${DISK}3"
 ROOT_PART="${DISK}4"
 
-echo "[*] Format the partition"
+echo "[*] Format partitions"
 mkfs.fat -F32 "$EFI_PART"
 mkfs.ext4 "$ROOT_PART"
 
-echo "[*] Mount partition"
+echo "[*] Mount partitions"
 mount "$ROOT_PART" /mnt
-mkdir -p /mnt/boot/efi
-mount "$EFI_PART" /mnt/boot/efi
+mkdir -p /mnt/boot
+mount "$EFI_PART" /mnt/boot
 
-echo "[*] Pacstrap base system"
+echo "[*] Install base system"
 pacstrap /mnt base linux linux-firmware
 
-echo "[*] Make fstab"
+echo "[*] Generate fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "[*] Copy the in_chroot.sh to the new system"
+echo "[*] Copy setup script into new system"
 cp in_chroot.sh /mnt/in_chroot.sh
 chmod +x /mnt/in_chroot.sh
 
 echo
-echo "[*] Okayyyyy, You have done the first step. Now run these following command:"
+echo "[*] Done pre-install! Now run:"
 echo "    arch-chroot /mnt"
-echo "    After that run the in_chroot.sh to finish"
+echo "    bash /root/in_chroot.sh"
